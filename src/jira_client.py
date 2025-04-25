75a8794592ae4c9eed04fd2aee53d4af60b6df97
Mate Stancsics <mate.stancsics@tngtech.com>
2025-04-25 11:15:24 +0200

    feat: add server to JIRA client environment variable validation

--- src/jira_client.py
+++ src/jira_client.py
@@ -9,6 +9,6 @@ def get_jira_client():
     jira_server = os.getenv('JIRA_SERVER')
     jira_email = os.getenv('JIRA_EMAIL')
     jira_api_key = os.getenv('JIRA_API_KEY')
-    if not jira_email or not jira_api_key:
-        raise ValueError("JIRA_EMAIL or JIRA_API_KEY is not set in the environment variables.")
+    if not jira_server or not jira_email or not jira_api_key:
+        raise ValueError("JIRA_SERVER, JIRA_EMAIL or JIRA_API_KEY is not set in the environment variables.")
     return JIRA(jira_server, basic_auth=(jira_email, jira_api_key))
